3036b6ff1873abeff3f64b19904dc90fba0c8551
WatIsDis31 <vincentiusedward31@gmail.com>
2025-12-15 17:51:45 +0700

    website update

--- script.js
+++ script.js
@@ -5,16 +5,80 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-const toggles = document.querySelectorAll(".theme-toggle");
+const toggle = document.querySelectorAll(".dark-toggle");
+const body = document.body;
 
-toggles.forEach((toggle) => {
-  toggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
+const savedTheme = localStorage.getItem("theme");
 
-    toggles.forEach((btn) => {
-      btn.textContent = document.body.classList.contains("dark")
-        ? "☀️"
-        : "🌙";
+if (savedTheme === "dark") {
+  body.classList.add("dark");
+}
+
+toggle.forEach(btn => {
+  btn.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    localStorage.setItem(
+      "theme",
+      body.classList.contains("dark") ? "dark" : "light"
+    );
+  });
+});
+
+const reveals = document.querySelectorAll(".reveal, .text-reveal");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
     });
+  },
+  {
+    threshold: 0.6,
+  },
+);
+
+document
+  .querySelectorAll(".letter-reveal")
+  .forEach((el) => observer.observe(el));
+
+reveals.forEach((el) => observer.observe(el));
+
+const letterElements = document.querySelectorAll(".letter-reveal");
+
+letterElements.forEach((el) => {
+  const text = el.textContent;
+  el.textContent = "";
+
+  [...text].forEach((char, i) => {
+    const span = document.createElement("span");
+    span.textContent = char === " " ? "\u00A0" : char;
+    span.style.transitionDelay = `${i * 0.04}s`;
+    el.appendChild(span);
+  });
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 120;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
   });
 });
